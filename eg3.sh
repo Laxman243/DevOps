3145d3e13ad53eec3fde5bbb84a1d2c80e04fcc7
laxman243 <laxmanshukla243@gmail.com>
2019-02-20 14:32:06 +0530

    message

--- eg3.sh
+++ eg3.sh
@@ -3,4 +3,19 @@ var=$( systemctl status jenkins )
 echo "$var" > "$hello.txt"
 cat hello.txt
 wc hello.txt
-awk 'FNR>=3' hello.txt
+
+grep ^Active:active hello.txt
+
+
+
+#in jenkin command shell script
+#!/bin/bash
+set -ex
+
+cd /var/lib/jenkins/workspace/hello
+
+bash 
+
+name="HELLO_WORLD"
+
+echo ${name}
